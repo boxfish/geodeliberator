a2b31c34538d8a6966c624eacf054ad4b753c001
Bo Yu <bohr.yu@gmail.com>
2012-11-09 15:06:50 -0500

    good to go

--- static/js/main.js
+++ static/js/main.js
@@ -1,5 +1,12 @@
-var mainLayout = null, contentLayout = null;
+var mainLayout = null,
+    contentLayout = null, 
+    contentCenterLayout = null, 
+    contentCenterCenterLayout = null,
+    contentEastLayout = null;
+
 var mapView = null;
+
+var currUserId = null;
 
 function initMainLayout() {
     mainLayout = $('body').layout({
@@ -34,24 +41,142 @@ function initContentLayout() {
         north__resizable: false,
         north__closable: false,
         north__spacing_open: 0,
-        //north__showOverflowOnHover:  true,
+        north__showOverflowOnHover:  true,
 
         center__paneSelector: ".center",
 
         east__paneSelector: ".east",
-        east__size: 350
+        east__size: 250
     });
 
-    $('#content .center').layout({
+    contentCenterLayout = $('#content-center').layout({
         name: "contentCenterLayout",
         applyDefaultStyles: false,
         
         center__paneSelector: ".center",
 
-        south__paneSelector: ".south",
-        south__size: 300
+        east__paneSelector: ".east",
+        east__size: 250,
+        east__initClosed: true
 
     });
+
+    contentCenterCenterLayout = $('#content-center .center').layout({
+        name: "contentCenterCenterLayout",
+        applyDefaultStyles: false,
+        
+        center__paneSelector: ".center",
+
+        south__paneSelector: ".south",
+        south__size: 150,
+        south__initClosed: true
+    });
+
+
+    contentEastLayout = $('#content-east').layout({
+        name: "contentEastLayout",
+        applyDefaultStyles: false,
+        
+        center__paneSelector: ".center",
+
+        south__paneSelector: ".south",
+        south__size: 350,
+        south__initClosed: true
+    });
+};
+
+// initialize the main toolbar for the forum
+function initForumTB() {
+    $('#forum_info').popover({
+        'title' : renderForumInfoTitle,
+        'content' : renderForumInfoContent,
+        'placement' : 'bottom',
+        'trigger' : 'click',
+        'template': '<div class="popover wide_popover"><div class="arrow"></div><div class="popover-inner"><h3 class="popover-title"></h3><div class="popover-content"><p></p></div></div></div>',
+        'html' : true
+    });
+    //$("#forum_info").click(onForumInfoClick);
+
+    $("#forum_timeline").click(onForumTimelineClick);
+    $("#forum_annotations").click(onForumAnnotationsClick);
+    $("#forum_history").click(onForumHistoryClick);
+    $("#forum_bookmarks").click(onForumBookmarksClick);
+    $("#forum_contribute").click(onForumContributeClick);
+    $("#forum_my_annotations").click(onForumMyAnnotationsClick);
+};
+
+function onForumInfoClick() {
+    if($(this).hasClass('active')) {
+        $('#forum_info').popover('hide');
+        //$('#forum_info').popover('destroy');
+    }
+    else {
+        $('#forum_info').popover('show');
+    }
+};
+
+function renderForumInfoTitle() {
+    return "Forum Title";
+};
+
+function renderForumInfoContent() {
+    return "details about the forum details about the forum details about the forum details about the forum";
+};
+
+function onForumTimelineClick() {
+    if($(this).hasClass('active')) {
+        contentCenterCenterLayout.close("south");
+    }
+    else {
+        contentCenterCenterLayout.open("south");
+    }
+    
+};
+
+function onForumAnnotationsClick() {
+    if($(this).hasClass('active')) {
+        contentCenterLayout.close("east");
+    }
+    else {
+        contentCenterLayout.open("east");
+    }
+};
+
+function onForumHistoryClick() {};
+
+function onForumBookmarksClick() {};
+
+function onForumContributeClick() {
+    if($(this).hasClass('active')) {
+        contentEastLayout.close("south");
+    }
+    else {
+        contentEastLayout.open("south");
+    }
+};
+
+function onForumMyAnnotationsClick() {};
+
+function initUserInfo() {
+    currUserId = null;
+    $('#user_banner').empty();
+    $('#signInOut').text('Sign In');
+    $('#signInOut').click(onSignInOutClick);
+    $('#sign_in_btn').click(onSignInBtnClick);
+};
+
+function onSignInOutClick() {
+    if (currUserId === null) {
+        // show the sign in window
+        $('#logInModal').modal('show');
+    }
+    else {
+        alert('sign out');
+    }
+};
+
+function onSignInBtnClick() {
+    alert('sign in');
 };
 
 function initGDMap() {
@@ -67,6 +192,8 @@ function showMainLayout() {
 $(document).ready(function(){
     initMainLayout();
     initContentLayout();
+    initUserInfo();
+    initForumTB();
     initGDMap();
 
     showMainLayout();
